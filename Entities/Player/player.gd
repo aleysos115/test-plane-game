52a218a81c957773956646e795c49ff3b5ed6fb9
aleysos115 <alex@kappa.localdomain>
2026-07-24 12:57:00 +0900

    Implemented basic fps controller and added in plane model

---
+++ Entities/Player/player.gd
@@ -0,0 +1,73 @@
+class_name Player extends CharacterBody3D
+
+@export_range(1, 35, 1) var speed: float = 10 # m/s
+@export_range(10, 400, 1) var acceleration: float = 100 # m/s^2
+
+@export_range(0.1, 3.0, 0.1) var jump_height: float = 1 # m
+@export_range(0.1, 3.0, 0.1, "or_greater") var camera_sens: float = 1
+
+@export var camera_point: Node3D
+
+var jumping: bool = false
+var mouse_captured: bool = false
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var move_dir: Vector2 # Input direction for movement
+var look_dir: Vector2 # Input direction for look/aim
+
+var walk_vel: Vector3 # Walking velocity 
+var grav_vel: Vector3 # Gravity velocity 
+var jump_vel: Vector3 # Jumping velocity
+
+var camera_root: Camera3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_capture_mouse()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		look_dir = event.relative * 0.001
+		if mouse_captured: _rotate_camera()
+	if Input.is_action_just_pressed(&"exit"): get_tree().quit()
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed(&"jump"): jumping = true
+	velocity = _walk(delta) + _gravity(delta) + _jump(delta)
+	move_and_slide()
+	camera_root.global_position = camera_point.global_position
+
+# PUBLIC FUNCTIONS
+
+func set_camera(camera: Camera3D) -> void:
+	camera_root = camera
+
+# PRIVATE SUPPORT FUNCTIONS
+
+func _capture_mouse() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	mouse_captured = true
+
+func _rotate_camera(sens_mod: float = 1.0) -> void:
+	camera_root.rotation.y -= look_dir.x * camera_sens * sens_mod
+	camera_root.rotation.x = clamp(camera_root.rotation.x - look_dir.y * camera_sens * sens_mod, -1.5, 1.5)
+
+func _walk(delta: float) -> Vector3:
+	move_dir = Input.get_vector(&"move_left", &"move_right", &"move_forward", &"move_backwards")
+	var _forward: Vector3 = camera_root.global_transform.basis * Vector3(move_dir.x, 0, move_dir.y)
+	var walk_dir: Vector3 = Vector3(_forward.x, 0, _forward.z).normalized()
+	walk_vel = walk_vel.move_toward(walk_dir * speed * move_dir.length(), acceleration * delta)
+	return walk_vel
+
+func _gravity(delta: float) -> Vector3:
+	grav_vel = Vector3.ZERO if is_on_floor() else grav_vel.move_toward(Vector3(0, velocity.y - gravity, 0), gravity * delta)
+	return grav_vel
+
+func _jump(delta: float) -> Vector3:
+	if jumping:
+		if is_on_floor(): jump_vel = Vector3(0, sqrt(4 * jump_height * gravity), 0)
+		jumping = false
+		return jump_vel
+	jump_vel = Vector3.ZERO if is_on_floor() or is_on_ceiling_only() else jump_vel.move_toward(Vector3.ZERO, gravity * delta)
+	return jump_vel
--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var main_camera: Camera3D 
+
 @export var level_root: Node3D
 @export var entity_root: Node3D
 @export var effect_root: Node3D
@@ -7,18 +9,27 @@ extends Node
 @export var target_scene: PackedScene
 @export var player_scene: PackedScene
 
+var player: Player
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if target_scene:
-		var scene_instance: Node = target_scene.instantiate()
+	load_level(target_scene)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func load_level(target: PackedScene) -> void:
+	if target:
+		var scene_instance: Node = target.instantiate()
 		level_root.add_child(scene_instance)
 	else:
 		push_warning("No scene assigned to target_scene variable!")
-		
+	
 	if player_scene:
-		var player_instance: Node = player_scene.instantiate()
-		entity_root.add_child(player_instance)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		player = player_scene.instantiate()
+		entity_root.add_child(player)
+		var spawn_point: Node3D = level_root.get_child(0).get_node("PlayerSpawn")
+		if spawn_point:
+			player.global_position = spawn_point.global_position
+		player.set_camera(main_camera)
